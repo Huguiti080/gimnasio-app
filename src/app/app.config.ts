a8741e08e32c61ec7faddfe44e70eebd95d86fd3
Hugo VH <vazquezhugo608@gmail.com>
2025-05-01 16:46:06 -0600

    Contenido de las opciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    importProvidersFrom(FontAwesomeModule), // Importa FontAwesomeModule
+    {
+      provide: FaIconLibrary,
+      useFactory: () => {
+        const library = new FaIconLibrary();
+        library.addIcons(faFacebook, faInstagram, faTwitter);
+        return library;
+      },
+    },
+  ],
 };
