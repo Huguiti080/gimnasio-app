dade2a5c7d25a4cc8cd2e653db370ded98eae6aa
Hugo VH <vazquezhugo608@gmail.com>
2025-05-03 16:55:03 -0600

    Implementacion del Login :)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { ClasesComponent } from './pages/clases/clases.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { authGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
@@ -11,5 +13,7 @@ export const routes: Routes = [
   { path: 'clases', component: ClasesComponent },
   { path: 'servicios', component: ServiciosComponent },
   { path: 'contacto', component: ContactoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: 'inicio' }
 ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  showError = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      const result = this.authService.login(username, password);
+
+      if (result.success) {
+        this.router.navigate(['/inicio']);
+      } else {
+        this.showError = true;
+        this.errorMessage = result.message;
+      }
+    } else {
+      this.showError = true;
+      this.errorMessage = 'Por favor, complete todos los campos requeridos.';
+    }
+  }
+}
